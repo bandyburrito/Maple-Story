1948b4b3e8fa1fb8855c626e02481f1cf0a69947
Ammar Rexhaj <167920633+bandyburrito@users.noreply.github.com>
2025-11-24 14:59:54 +0100

    Push #4
    Added Inventory Selecting Items and Swapping Function & Working on the new parts of the game

--- Assets/Scripts/movement.cs
+++ Assets/Scripts/movement.cs
@@ -1,43 +1,45 @@
 using System;
 using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.UI;
 
 
 public class PlayerMovement : MonoBehaviour
 {
-    [Header("Movement Settings")]
+    
     public float moveSpeed = 5f;
-
     private Rigidbody2D rb;
     private Vector2 movementInput;
-
     public float dashForce = 50f;
-
-    public GameObject Bullet;
-
-    private float rotation;
-    private bool BulletShot;
-
+    public GameObject PistolBullet;
+    public GameObject ShotgunBullet;
+    private bool pistolequipped;
+    private bool shotgunequippped;
+    public Image item1;
+    public Image item2;
 
 
     void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
-
         gameObject.GetComponent<GameObject>();
-
         gameObject.GetComponent<Transform>();
+        pistolequipped = true; 
+        Item2Color();
+        
+    }
 
-
+    void Start()
+    {
+            
     }
 
     void Update()
     {
         
-
         movementInput.x = Input.GetAxisRaw("Horizontal");
         movementInput.y = Input.GetAxisRaw("Vertical");
         movementInput = movementInput.normalized;
-
 
         if (Input.GetKeyDown(KeyCode.LeftShift))
         {
@@ -49,24 +51,62 @@ public class PlayerMovement : MonoBehaviour
             moveSpeed = moveSpeed / 2f;
         }
 
-
-        if (Input.GetButtonDown("Fire1"))
+        if (Input.GetKeyDown(KeyCode.Alpha1))
         {
-            Instantiate(Bullet, transform.position, transform.rotation);
+            pistolequipped = true;
+            shotgunequippped = false;
+            Item2Color();
         }
 
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            pistolequipped = false;
+            shotgunequippped = true;
+            Item1Color();
+        }
+
+    
+        if (Input.GetButtonDown("Fire1") && pistolequipped == true)
+        {
+            Instantiate(PistolBullet, transform.position, transform.rotation);
+        }
+
+        if (Input.GetButtonDown("Fire1") && shotgunequippped == true)
+        {
+            Instantiate(ShotgunBullet, transform.position, transform.rotation);
+        }
+        
         if (Input.GetKeyDown(KeyCode.Space))
         {
             rb.AddForce(movementInput * dashForce);    
-            
         }
-        
-        
-        
+
     }
     void FixedUpdate()
     {
         rb.linearVelocity = movementInput * moveSpeed * Time.deltaTime;
+    }
+
+    private void Item2Color()
+    {
+        Color tempColor1 = item1.color;
+        tempColor1.a = 0.35f;
+        item1.color = tempColor1;
+
+        Color tempColor2 = item2.color;
+        tempColor2.a = 1f;
+        item2.color = tempColor2;
+    }
+    
+    private void Item1Color()
+    {
+        Color tempColor2 = item2.color;
+        tempColor2.a = 0.35f;
+        item2.color = tempColor2;
+
+        Color tempColor1 = item1.color;
+        tempColor1.a = 1f;
+        item1.color = tempColor1;
     }
         
     
